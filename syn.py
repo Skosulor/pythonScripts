d0926a1fd9acec91a88dd46fb5e9635f967bb9c4
andreas <andreas.ohman@gmail.com>
2020-04-08 10:24:48 +0200

    added syn.py

---
+++ syn.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import re
+from bs4 import BeautifulSoup
+
+
+
+def getSynonyms(word, recursive):
+    w = word
+    page = requests.get('https://www.thesaurus.com/browse/' + word)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    texts = soup.find_all('a')
+
+    tt = []
+
+    for t in texts:
+        tt.append(t.get_text())
+        # print(t.get_text())
+
+    start = [ i for i, word in enumerate(tt) if word.startswith('SEE DEFINITION OF') ]
+    try:
+        end   = [ i for i, word in enumerate(tt) if word.startswith('PREVIOUS') ]
+    except:
+        end   = [ i for i, word in enumerate(tt) if word.startswith('OTHERS ARE READING') ]
+
+    if end == []:
+        end   = [ i for i, word in enumerate(tt) if word.startswith('OTHERS ARE READING') ]
+
+    try:
+        for t in reversed(tt[start[0]+1:end[0]]):
+            print(t)
+    except:
+        print('Could not find synonyms, maybe the word is misspelled?')
+
+try:
+    word = str(sys.argv[1])
+except:
+    print("No input arguments")
+    exit()
+getSynonyms(word, True)
+# print(start)
+# print(end)
